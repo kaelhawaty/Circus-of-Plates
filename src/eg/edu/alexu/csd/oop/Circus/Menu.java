d167921ec753e86e2e5dadbc87deba3aade09e91
Karim Ahmed <kaelhawaty@gmail.com>
2019-12-19 22:14:55 +0200

    Fixed Reading Menu Image

--- src/eg/edu/alexu/csd/oop/Circus/Menu.java
+++ src/eg/edu/alexu/csd/oop/Circus/Menu.java
@@ -1,18 +1,20 @@
 package eg.edu.alexu.csd.oop.Circus;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Image;
+import java.io.IOException;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 
 public class Menu {
     String currentLevel="";
-    public void getLevel() {
+    public void getLevel() throws IOException {
         JFrame frame;
         frame = new JFrame();
         frame.setBounds(100, 100, 306, 329);
@@ -22,7 +24,7 @@ public class Menu {
         JLabel lblNewLabel = new JLabel("");
         lblNewLabel.setBounds(0, 0, 300, 300);
         lblNewLabel.setForeground(Color.BLACK);
-        Image img=new ImageIcon("menu3.jpg").getImage();
+        Image img= ImageIO.read(getClass().getClassLoader().getResourceAsStream("menu3.jpg"));
         frame.getContentPane().setLayout(null);
         lblNewLabel.setIcon(new ImageIcon(img));
         frame.getContentPane().add(lblNewLabel);
--- src/eg/edu/alexu/csd/oop/Circus/main.java
+++ src/eg/edu/alexu/csd/oop/Circus/main.java
@@ -6,9 +6,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Menu m=new Menu();
         m.getLevel();
     }
