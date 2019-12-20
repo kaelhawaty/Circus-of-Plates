ab6740ddc7b3c7b6a7e3e626941a6db0258cde0c
HazemShawky <hazemahmed575@gmail.com>
2019-12-20 23:18:00 +0200

    jar

--- src/eg/edu/alexu/csd/oop/Circus/Audio.java
+++ src/eg/edu/alexu/csd/oop/Circus/Audio.java
@@ -1,26 +1,24 @@
 package eg.edu.alexu.csd.oop.Circus;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
 import java.io.File;
+import java.io.IOException;
 
 public class Audio {
     Clip clip;
     public void playMusic(String audioLocation){
         try {
-            File musicPath=new File(audioLocation);
-            if(musicPath.exists()){
-                AudioInputStream input= AudioSystem.getAudioInputStream(musicPath);
-                clip=AudioSystem.getClip();
-                clip.open(input);
-                clip.start();
-                clip.loop(Clip.LOOP_CONTINUOUSLY);
-            }
-            else
-                System.out.println("can't find the file");
-        }catch (Exception e){
-            e.printStackTrace();
+            AudioInputStream input= AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream(audioLocation));;
+            clip=AudioSystem.getClip();
+            clip.open(input);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            } catch (LineUnavailableException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (UnsupportedAudioFileException ex) {
+            ex.printStackTrace();
         }
     }
     public void stop(){ clip.stop();}
